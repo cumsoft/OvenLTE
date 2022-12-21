5078a2022bb4604c1c1ba2eb5b0f8f030947c7b7
Cumsoft™ <97250816+cumsoft@users.noreply.github.com>
2022-12-21 15:09:44 -0500

    Update and rename cpumonitoropti.py to Cumsoft_OvenLTE_Dev.py

--- Cumsoft_OvenLTE_Dev.py
+++ Cumsoft_OvenLTE_Dev.py
@@ -28,62 +28,28 @@
 #################################################################
 #################################################################
 
-
-import tkinter as tk
-import subprocess
-
-# Set the initial temperature thresholds
-low_threshold = 24
-high_threshold = 30
-
-# Function to get the CPU temperature and update the temperature display
-def update_temp():
-    # Run the vcgencmd measure_temp command and parse the output to get the temperature
-    output = subprocess.run(['vcgencmd', 'measure_temp'], capture_output=True).stdout.decode()
-    temp = float(output.split('=')[1].split("'")[0])
-
-    # Set the color of the temperature label based on the temperature thresholds
-    if temp < low_threshold:
-        temp_label.config(fg='green')
-    elif temp < high_threshold:
-        temp_label.config(fg='yellow')
-    else:
-        temp_label.config(fg='red')
-
-    # Update the temperature label with the current temperature
-    temp_label.config(text=f'Temperature: {temp:.1f}°C')
-
-    # Schedule the update_temp function to be called again after 1000 milliseconds (1 second)
-    root.after(1000, update_temp)
-
-# Create the main window
-root = tk.Tk()
-root.title('CPU Temperature Monitor')
-
-# Create the temperature label
-temp_label = tk.Label(root, text='Temp:', font=('Arial', 15))
-temp_label.pack()
-
-# Create the input fields for setting the temperature thresholds
-low_threshold_entry = tk.Entry(root)
-low_threshold_entry.insert(0, low_threshold)
-low_threshold_entry.pack()
-
-high_threshold_entry = tk.Entry(root)
-high_threshold_entry.insert(0, high_threshold)
-high_threshold_entry.pack()
-
-# Create the button for setting the temperature thresholds
-def set_thresholds():
-    global low_threshold, high_threshold
-    low_threshold = float(low_threshold_entry.get())
-    high_threshold = float(high_threshold_entry.get())
-
-set_button = tk.Button(root, text='Set Thresholds', command=set_thresholds)
-set_button.pack()
-
-# Start the temperature update loop
-update_temp()
-
-# Run the Tkinter event loop
-root.mainloop()
+I=float
+import tkinter as B,subprocess as J
+D=24
+E=30
+def H():
+	F=J.run(['vcgencmd','measure_temp'],capture_output=True).stdout.decode();B=I(F.split('=')[1].split("'")[0])
+	if B<D:C.config(fg='green')
+	elif B<E:C.config(fg='yellow')
+	else:C.config(fg='red')
+	C.config(text=f"Temperature: {B:.1f}°C");A.after(1000,H)
+A=B.Tk()
+A.title('CPU Temperature Monitor')
+C=B.Label(A,text='Temp:',font=('Arial',15))
+C.pack()
+F=B.Entry(A)
+F.insert(0,D)
+F.pack()
+G=B.Entry(A)
+G.insert(0,E)
+G.pack()
+def K():global D,E;D=I(F.get());E=I(G.get())
+L=B.Button(A,text='Set Thresholds',command=K)
+L.pack()
+H()
+A.mainloop()
