ffc284a37431bec695001067fc76e0a0beb3b83c
Cumsoft™ <97250816+cumsoft@users.noreply.github.com>
2022-12-24 22:53:05 -0500

    Update Cumsoft_OvenLTE_App.py

--- Cumsoft_OvenLTE_App.py
+++ Cumsoft_OvenLTE_App.py
@@ -32,7 +32,7 @@
 
 import tkinter as B,subprocess as A
 class C(B.Tk):
-	def __init__(A):B.Tk.__init__(A);A.title('CPU Temperature Gauge');A.temperature_gauge=B.Canvas(A,width=200,height=100,bg='white');A.temperature_gauge.pack();A.temperature_label=B.Label(A,text='Temperature: N/A',font=('Helvetica',16));A.temperature_label.pack();A.update_button=B.Button(A,text='Turn On',command=A.update_temperature);A.update_button.pack();A.after(5000,A.update_temperature)
+	def __init__(A):B.Tk.__init__(A);A.title('Cumsoft_OvenLTE_App');A.temperature_gauge=B.Canvas(A,width=200,height=100,bg='white');A.temperature_gauge.pack();A.temperature_label=B.Label(A,text='Temperature: N/A',font=('Helvetica',16));A.temperature_label.pack();A.update_button=B.Button(A,text='Reset',command=A.update_temperature);A.update_button.pack();A.after(5000,A.update_temperature)
 	def update_temperature(A):
 		B='temp';D=A.get_cpu_temperature();A.temperature_label.config(text=f"Temperature: {D}°C");C=D/100.0;A.temperature_gauge.delete(B)
 		if C>0.75:A.temperature_gauge.create_rectangle(0,0,200,100,fill='red',tags=B)
